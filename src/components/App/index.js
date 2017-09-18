0e741ef69f25756959eb606a5bc9dd5e701cd5ec
chabab hassan <chababhassan@gmail.com>
2017-09-18 15:20:55 +0100

    Built HeroSection component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Header from "../Header";
+import HeroSection from "../HeroSection";
 
 export default class App extends Component {
 
@@ -10,7 +11,8 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <Header/>
+        <Header />
+        <HeroSection />
       </div>
     );
   }
---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import './hero_section.css';
+import me from '../../images/me.jpg';
+
+class HeroSection extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <section className="hf-hero">
+        {/* avatar iamge */}
+        <div className="hf-hero__avatar">
+          <div className="hf-hero__avatar-large">
+            <img src={me} alt=""/>
+          </div>
+        </div>
+
+        {/* content wrapper */}
+        <div className="hf-hero__content">
+          {/* hero title */}
+          <div className="hf-hero__title">
+            Date Night Restaurants
+          </div>
+
+          {/* hero description */}
+          <div className="hf-hero__description">
+            <h3>Best places for a date night in New York City</h3>
+          </div>
+
+          {/* follow section */}
+          <div className="hf-hero__follow">
+            {/* followers number */}
+            <div className="hf-hero__follow-number">
+              6M followers
+            </div>
+            <div className="flex-grow"></div>
+            {/* follow button */}
+            <div className="hf-hero__follow-left">
+              <span className="wx"><button className="btn btn-primary">follow</button></span>
+              <span className="hf-icon"><i data-feather="settings"/></span>
+            </div>
+          </div>
+
+        </div>
+      </section>
+    )
+  }
+
+}
+
+export default HeroSection;
