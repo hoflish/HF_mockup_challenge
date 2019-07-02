5788fd15a4c0a3daf04ab7a5ab8e0d74fdcc27db
Hassan Chabab <chababhassan@gmail.com>
2019-07-02 17:38:39 +0100

    refactor: convert PasswordResetForm to functional component

--- src/components/PasswordResetForm/index.js
+++ src/components/PasswordResetForm/index.js
@@ -1,34 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { FormField } from "..";
+import { useFirebase } from "../../context/firebase-context";
 
-const INITIAL_STATE = {
+const initialState = {
   email: "",
   error: ""
 };
 
-class PasswordResetForm extends React.Component {
-  state = {
-    ...INITIAL_STATE
-  };
+const PasswordResetForm = ({ onEmailSent }) => {
+  const [form, setForm] = useState(initialState);
+  const { firebase } = useFirebase();
 
-  onChangeEmail = event => {
-    this.setState({ email: event.target.value, error: "" });
-  };
+  function onChange(event) {
+    setForm({ email: event.target.value, error: "" });
+  }
 
-  onSubmit = event => {
-    const { email } = this.state;
-    const { firebase, onEmailSent } = this.props;
+  function onSubmit(event) {
     event.preventDefault();
 
-    if (!email) {
-      this.setState({ error: "Please enter your email" });
+    if (!form.email) {
+      setForm({ ...form, error: "Please enter your email" });
       return;
     }
 
     firebase
-      .doPasswordReset(email)
+      .doPasswordReset(form.email)
       .then(() => {
+        // TODO: use global notification
+        setForm(initialState);
         onEmailSent &&
           onEmailSent(
             "We've sent you an email with a link to reset your password."
@@ -38,31 +38,28 @@ class PasswordResetForm extends React.Component {
         // TODO: handle errors
         // see https://firebase.google.com/docs/reference/js/firebase.auth.Auth.html#sendpasswordresetemail
         const { message } = error;
-        this.setState({ error: message });
+        setForm({ ...form, error: message });
       });
-  };
-
-  render() {
-    const { email, error } = this.state;
-    return (
-      <form onSubmit={this.onSubmit}>
-        <FormField
-          name="email"
-          value={email}
-          placeholder="Email"
-          error={error}
-          onChange={this.onChangeEmail}
-        />
-        <button
-          className="button is-primary"
-          type="submit"
-          style={{ float: "right" }}
-        >
-          Submit
-        </button>
-      </form>
-    );
   }
-}
+
+  return (
+    <form onSubmit={onSubmit}>
+      <FormField
+        name="email"
+        value={form.email}
+        placeholder="Email"
+        error={form.error}
+        onChange={onChange}
+      />
+      <button
+        className="button is-primary is-fullwidth "
+        type="submit"
+        style={{ float: "right" }}
+      >
+        Send password reset email
+      </button>
+    </form>
+  );
+};
 
 export default PasswordResetForm;
